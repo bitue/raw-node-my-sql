58f749fc7807ec9ffb959534f50898e0815be881
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-11-20 17:24:46 +0600

    add sql

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 const mysql = require('mysql');
-const http = require('http')
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json())
 
-const dataBaseConfig ={
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'school3'
+
+
+const dataBaseConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'school3'
 }
+
+
 
 
 const connection = mysql.createConnection(dataBaseConfig);
 
-connection.connect((err)=> {
-    if(err){
+connection.connect((err) => {
+    if (err) {
         console.log('failed')
     }
-    else{
+    else {
         console.log('success');
-       
+
         // deleteFunc(connection)
         //updataData(connection)
         // deleteData(connection)
-        
-        selectData(connection);
+
+
+        // ---------------ALL OF THE THING IS DONE BY THIS WAY FOR SQL CRUD------------------------------
+
+        app.get('/select', (req, res) => {
+            const sqlQuery = "SELECT * FROM `students`"
+            connection.query(sqlQuery, (err, result) => {
+                if (err) {
+                    console.log(err.message)
+                }
+                else {
+                    res.send(result);
+                }
+            })
+
+        })
+
+        // selectData(connection);
         // insertData(connection)
 
 
@@ -31,17 +54,17 @@ connection.connect((err)=> {
 
 
 
-function insertData (con) {
+function insertData(con) {
     const sqlQuery = "INSERT INTO `students`( `name`, `city`, `phone`, `father`) VALUES ('bitue','chittagong', '01111', 'mmmm')";
-    con.query(sqlQuery , (err)=> {
-        if(err){
+    con.query(sqlQuery, (err) => {
+        if (err) {
             console.log(err.message)
 
         }
-        else{
+        else {
             console.log('inserted done')
         }
-       
+
     })
 
 }
@@ -49,50 +72,64 @@ function insertData (con) {
 
 
 
-function updataData (con) {
+function updataData(con) {
     const sqlQuery = "UPDATE `students` SET `phone`='500' WHERE id='2'"
-    con.query(sqlQuery , (err)=> {
-        if(err){
+    con.query(sqlQuery, (err) => {
+        if (err) {
             console.log(err.message)
 
         }
-        else{
+        else {
             console.log('inserted done')
         }
-       
+
     })
 
 }
 
 
-function deleteData (con) {
+function deleteData(con) {
     const sqlQuery = "DELETE FROM `students` WHERE id='2'"
-    con.query(sqlQuery , (err)=> {
-        if(err){
+    con.query(sqlQuery, (err) => {
+        if (err) {
             console.log(err.message)
 
         }
-        else{
+        else {
             console.log('deleted done')
         }
-       
+
     })
 
 }
 
 
 function selectData(con) {
-    const sqlQuery ="SELECT `id`, `name`, `city`, `phone`, `father` FROM `students` WHERE id='7'"
-    con.query(sqlQuery, (err, result)=> {
-        if(err){
+    const sqlQuery = "SELECT * FROM `students`"
+    con.query(sqlQuery, (err, result) => {
+        if (err) {
             console.log(err.message)
         }
-        else{
+        else {
             console.log(result)
+            return result
         }
     })
-    
+
 }
+
+
+app.get('/', (req, res) => {
+    res.send('i am runnuing')
+})
+
+app.listen(5001, () => {
+    console.log('app is running at prot', 5001)
+})
+
+
+
+
 
 
 
